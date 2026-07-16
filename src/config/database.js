0480e11e73e0276b1b1b769d4f79c15b7f7eabf6
deepak shrivas <146703548+Deepakshrivas07@users.noreply.github.com>
2026-07-16 18:11:51 +0530

    Delete src/config directory

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://deepakshrivas440:deepak123@nodepractice.kysiuez.mongodb.net/Devtinder');
-}
-
-module.exports = {
-    connectDB,
-}
